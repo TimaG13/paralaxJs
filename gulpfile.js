a3c8a0a7d9e406b111e8c6f160af4c5d6749820c
Тимофей Гуменюк <tima.coc13@gmail.com>
2020-07-15 15:44:28 +0300

    fix err

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp                 = require('gulp'),
+      sass                 = require('gulp-sass'),
+      sourcemaps           = require('gulp-sourcemaps'),
+      watch                = require('gulp-watch'),
+      autoprefixer         = require('gulp-autoprefixer'),
+      concat               = require('gulp-concat'),
+      cleanCSS             = require('gulp-clean-css'),
+      uglify               = require('gulp-uglify'),
+      overrideBrowserslist = require('browser-sync').create(),
+      cssFiles             = [
+          'sass/style.scss'
+      ],
+      jsFiles              = [
+          'sass/script.js'
+      ];
+// all css
+gulp.task('sass',function () {
+   return gulp.src(cssFiles)
+       .pipe(sourcemaps.init())
+       .pipe(autoprefixer({
+           browsers: ['last 2 versions'],
+           cascade: false
+       }))
+       .pipe(sass().on('error',sass.logError))
+       .pipe(concat('styles.min.css'))
+       .pipe(cleanCSS({
+           level: 2
+       }))
+       .pipe(sourcemaps.write())
+       .pipe(gulp.dest('dist/css/'))
+       .pipe(overrideBrowserslist.stream())
+});
+// all js
+gulp.task('js',function () {
+    return gulp.src(jsFiles)
+        .pipe(sourcemaps.init())
+        .pipe(uglify({
+            toplevel: true
+        }))
+        .pipe(concat('scripts.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(overrideBrowserslist.stream())
+});
+
+
+gulp.task('watch',function () {
+   gulp.watch('sass/**/*.scss',gulp.series('sass'));
+   gulp.watch('sass/**/*.js',gulp.series('js'));
+   gulp.watch("./**/*.html").on('change', overrideBrowserslist.reload);
+   gulp.watch("./**/*.php").on('change', overrideBrowserslist.reload);
+    overrideBrowserslist.init({
+        proxi: "",
+        notify: false
+    });
+});
--- sass/script.js
+++ sass/script.js
@@ -1,30 +1,5 @@
-
 //scroll
-$(function () {
 
-    var $window = $(window);		//Window object
-
-    var scrollTime = 0.5;			//Scroll time
-    var scrollDistance = 200;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
-
-    $window.on("mousewheel DOMMouseScroll", function (event) {
-
-        event.preventDefault();
-
-        var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
-        var scrollTop = $window.scrollTop();
-        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
-
-        TweenMax.to($window, scrollTime, {
-            scrollTo: {y: finalScroll, autoKill: false},
-            ease: Power1.easeOut,
-            autoKill: true,
-            overwrite: 5
-        });
-
-    });
-
-});
 $(document).ready(function () {
 
     // anim rand text
